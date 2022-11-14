04f84f64f558765ba6699284a011b21a30cea1f5
JungHoe <dlwjdghl159@naver.com>
2022-11-14 13:49:32 +0900

    section2 enum 타입

--- section2/app.js
+++ section2/app.js
@@ -2,14 +2,30 @@
 //   name: "junghoe",
 //   age: 30,
 // };
+// const person: //타입 추론이 명확히 가능할때는 타입을 명시하지 않는것이 가독성이 좋다.
+// { name: string; age: number; hobbies: string[]; role: [number, string] } = {
+//   name: "junghoe",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [1, "author"],
+// };
+// // person.role.push("admin"); ㅈ바 스크립트여서 push 를 막을순없음 ㅠ.ㅠ
+// // person.role[1] = 10; 튜플 타입 인자 오류로 컴파일 에러발생
+// const ADMIN = 0;
+// const READ_ONLY_USER = 1;
+// const AUTHOR_USER = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY_USER"] = 1] = "READ_ONLY_USER";
+    Role[Role["AUTHOR_USER"] = 2] = "AUTHOR_USER";
+})(Role || (Role = {}));
 var person = {
     name: "junghoe",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [1, "author"]
+    role: Role.ADMIN
 };
-// person.role.push("admin"); ㅈ바 스크립트여서 push 를 막을순없음 ㅠ.ㅠ
-// person.role[1] = 10; 튜플 타입 인자 오류로 컴파일 에러발생
 console.log(person.name);
 var favoriteActivities;
 favoriteActivities = ["tennis", "soccer"];
@@ -17,4 +33,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var item = _a[_i];
     item.toUpperCase();
     console.log(item);
+}
+if (person.role === Role.ADMIN) {
+    console.log("관리자입니다.");
 }
--- section2/app.ts
+++ section2/app.ts
@@ -2,16 +2,35 @@
 //   name: "junghoe",
 //   age: 30,
 // };
-const person: //타입 추론이 명확히 가능할때는 타입을 명시하지 않는것이 가독성이 좋다.
+// const person: //타입 추론이 명확히 가능할때는 타입을 명시하지 않는것이 가독성이 좋다.
 
-{ name: string; age: number; hobbies: string[]; role: [number, string] } = {
+// { name: string; age: number; hobbies: string[]; role: [number, string] } = {
+//   name: "junghoe",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [1, "author"],
+// };
+// // person.role.push("admin"); ㅈ바 스크립트여서 push 를 막을순없음 ㅠ.ㅠ
+// // person.role[1] = 10; 튜플 타입 인자 오류로 컴파일 에러발생
+
+// const ADMIN = 0;
+// const READ_ONLY_USER = 1;
+// const AUTHOR_USER = 2;
+
+// javascript enum 에서는 0부터 다음값을 순차적으로 증가시키고 최초값을 할당하면 해당값부터 증가시킴 ex) ADMIN=5 ..6 7
+// 값을 임의로정의하면 별도로 사용도 가능함. ex) ADMIN= 5 ,AUTHOR_USER='author'
+enum Role {
+  ADMIN,
+  READ_ONLY_USER,
+  AUTHOR_USER,
+}
+
+const person = {
   name: "junghoe",
   age: 30,
   hobbies: ["Sports", "Cooking"],
-  role: [1, "author"],
+  role: Role.ADMIN,
 };
-// person.role.push("admin"); ㅈ바 스크립트여서 push 를 막을순없음 ㅠ.ㅠ
-// person.role[1] = 10; 튜플 타입 인자 오류로 컴파일 에러발생
 
 console.log(person.name);
 let favoriteActivities: string[];
@@ -20,4 +39,8 @@ favoriteActivities = ["tennis", "soccer"];
 for (const item of person.hobbies) {
   item.toUpperCase();
   console.log(item);
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("관리자입니다.");
 }
