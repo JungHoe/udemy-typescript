cf5f83896d199a5adfb44f1094b3eb498c327a13
JungHoe <dlwjdghl159@naver.com>
2022-11-14 14:43:00 +0900

    section2 리터럴타입
    * const= 값이 변하지않는 변수
    * literal 변하지 않는 데이터

--- section2/app.js
+++ section2/app.js
@@ -1,14 +1,23 @@
-var combine = function (a, b) {
+//상수와 리터럴의 차이를 생각하자
+var combine = function (a, b, resultType //리터럴타입
+) {
     var result;
-    if (typeof a === "number" && typeof b === "number") {
-        result = a + b;
+    if ((typeof a === "number" && typeof b === "number") ||
+        resultType === "as-number") {
+        result = +a + +b;
     }
     else {
         result = a.toString() + b.toString();
     }
     return result;
+    //   if (resultType === "as-number") {
+    //     return +result;
+    //   }
+    //   return result.toString();
 };
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.debug("🤔 ~ file: app.ts ~ line 9 ~ combinedAges", combinedAges);
-var combinedNames = combine("Jung", "Hoe");
+var combinedStringAges = combine("30", "26", "as-number");
+console.debug("🤔 ~ file: app.ts ~ line 21 ~ combinedStringAges", combinedStringAges);
+var combinedNames = combine("Jung", "Hoe", "as-text");
 console.debug("🤔 ~ file: app.ts ~ line 17 ~ combinedNames", combinedNames);
--- section2/app.ts
+++ section2/app.ts
@@ -1,17 +1,37 @@
-const combine = (a: number | string, b: number | string) => {
+//상수와 리터럴의 차이를 생각하자
+
+const combine = (
+  a: number | string,
+  b: number | string,
+  resultType: "as-number" | "as-text" //리터럴타입
+) => {
   let result: any;
-  if (typeof a === "number" && typeof b === "number") {
-    result = a + b;
+  if (
+    (typeof a === "number" && typeof b === "number") ||
+    resultType === "as-number"
+  ) {
+    result = +a + +b;
   } else {
     result = a.toString() + b.toString();
   }
   return result;
+  //   if (resultType === "as-number") {
+  //     return +result;
+  //   }
+  //   return result.toString();
 };
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 
 console.debug("🤔 ~ file: app.ts ~ line 9 ~ combinedAges", combinedAges);
 
-const combinedNames = combine("Jung", "Hoe");
+const combinedStringAges = combine("30", "26", "as-number");
+
+console.debug(
+  "🤔 ~ file: app.ts ~ line 21 ~ combinedStringAges",
+  combinedStringAges
+);
+
+const combinedNames = combine("Jung", "Hoe", "as-text");
 
 console.debug("🤔 ~ file: app.ts ~ line 17 ~ combinedNames", combinedNames);
