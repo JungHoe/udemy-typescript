2468f53ce9100e440130939537ee887149e6cca3
JungHoe <dlwjdghl159@naver.com>
2022-12-19 21:17:08 +0900

    section2 never 타입

--- section2/app.js
+++ section2/app.js
@@ -1,3 +1,15 @@
 var userInput;
+// let userInput: any;
+// any 는 타입검사를 아얘안해버림
+var userName;
 userInput = 5;
 userInput = "Max";
+// userName = userInput;
+// 추론이 가능한경우 unknown 보단 union을 권장
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+function generateError(msg, code) {
+    throw { message: msg, errorCode: code };
+}
+generateError("somethings wrong", 404);
--- section2/app.ts
+++ section2/app.ts
@@ -9,6 +9,14 @@ userInput = "Max";
 
 // userName = userInput;
 
+// 추론이 가능한경우 unknown 보단 union을 권장
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+// 절대적으로 반환하지않음 void와 비슷 그러나 충돌이나 오류의 가능성을 명시함
+function generateError(msg: string, code: number): never {
+  throw { message: msg, errorCode: code };
+}
+
+generateError("somethings wrong", 404);
