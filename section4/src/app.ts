9aa7bae31cf8bd6924e0604d0f3a4c04347d5026
JungHoe <dlwjdghl159@naver.com>
2022-12-29 12:30:56 +0900

    section4 variable hoisting

--- section4/src/app.ts
+++ section4/src/app.ts
@@ -1,1 +1,22 @@
-console.log("test");
+console.log("good");
+const userName: string = "junghoe";
+
+let age = 25;
+age = 27;
+
+// function add(a: number, b: number) {
+//   const result = a + b;
+//   return result;
+// }
+
+function add(a: number, b: number) {
+  var result = a + b;
+  return result;
+}
+
+if (true) {
+  var test = true;
+  let test2 = true;
+}
+console.log(test);
+console.log(test2);
