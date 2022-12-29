cfa2ba1b96e597e701fc1ab0ad99c1b1560eee1e
JungHoe <dlwjdghl159@naver.com>
2022-12-29 14:46:57 +0900

    section5 readonly

--- section5/src/app.ts
+++ section5/src/app.ts
@@ -4,7 +4,7 @@ class Department {
   // private name: string;
   private employees: string[] = [];
 
-  constructor(private id: string, private name: string) {}
+  constructor(private readonly id: string, private name: string) {}
   describe(this: Department) {
     console.log(`department ::: ${this.id}__${this.name}`);
   }
