e22060e56251a4cb32e6e150bc946805b4e0ce4e
JungHoe <dlwjdghl159@naver.com>
2022-12-29 14:24:21 +0900

    section5 class 접근제어
     * private 로 외부접근 제어
      ** ts에서만 제공 실제 Js 구현안됨

--- section5/src/app.ts
+++ section5/src/app.ts
@@ -2,11 +2,20 @@ console.log("good");
 
 class Department {
   name: string;
+  private employees: string[] = [];
   constructor(initName: string) {
     this.name = initName;
   }
   describe(this: Department) {
     console.log("department :::" + this.name);
+  }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+  printEmployee() {
+    console.log(this.employees.length);
+
+    console.log(this.employees);
   }
 }
 
@@ -15,7 +24,10 @@ const it = new Department("IT");
 console.debug("🤔 ~ file: app.ts:12 ~ it", it);
 
 it.describe();
+it.addEmployee("Max");
+it.addEmployee("Min");
+it.printEmployee();
 
-const itCopy = { name: "dummy", describe: it.describe };
+// const itCopy = { name: "dummy", describe: it.describe };
 
-itCopy.describe();
+// itCopy.describe();
