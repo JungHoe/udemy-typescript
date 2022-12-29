1271b46488d5a82f32ac20aa6d896426a4f3cde5
JungHoe <dlwjdghl159@naver.com>
2022-12-29 12:55:47 +0900

    section4 arrow function
     * binidng 자동화
     * 기본값 함수

--- section4/src/app.ts
+++ section4/src/app.ts
@@ -9,14 +9,28 @@ age = 27;
 //   return result;
 // }
 
-function add(a: number, b: number) {
-  var result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   var result = a + b;
+//   return result;
+// }
 
-if (true) {
-  var test = true;
-  let test2 = true;
-}
-console.log(test);
-console.log(test2);
+// if (true) {
+//   var test = true;
+//   let test2 = true;
+// }
+// console.log(test);
+// console.log(test2);
+
+const add = (a: number, b: number = 0) => {
+  return a + b;
+};
+
+const printf = (output: string | number): void => {
+  console.log(output);
+};
+
+const buttonDom = document.body.querySelector("button")!;
+buttonDom.addEventListener("click", (e) => {
+  console.log(e);
+});
+printf(add(5));
