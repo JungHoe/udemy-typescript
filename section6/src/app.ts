7266fe4afb62503a91f369a5cd73a2bbc35657ee
JungHoe <junghwi0603@gmail.com>
2023-01-12 11:07:25 +0900

    section6 intersection type

--- section6/src/app.ts
+++ section6/src/app.ts
@@ -1,39 +1,26 @@
-// type Person = {
-//   name: string;
-//   age: number;
-//   greet(phrase: string): void;
-// };
-
-// type과 동일해 보이지만 interface는 객체의 구조를 설명하기 위해서만 사용한다
-
-// type addFunc = (a:number,b:number)=>number;
-interface addFunc {
-  (a: number, b: number): number;
-}
-
-let add: addFunc;
-
-add = (a: number, b: number) => {
-  return a + b;
+type Admin = {
+  name: string;
+  priviliges: string[];
 };
-interface Named {
-  readonly name: string;
-  outputName?: string; // 옵션으로 구현 여부 지정
+
+
+type Employee = {
+  name:string;
+  startDate : Date;
 }
-interface Greetable extends Named {
-  greet(phrase: string): void;
+// & 연산자로 intersection 타입을 선언할수있다.
+type ElevatedEmployee = Admin & Employee;
+// interface 로 구현시 위 2가지 타입을 extends 받는것과 동일하다.
+
+
+const e1 : ElevatedEmployee = {
+  name : 'Junghoe',
+  priviliges: ['create-server'],
+  startDate: new Date();
 }
 
-class Person implements Greetable, Named {
-  constructor(public name: string) {}
-
-  greet(this: Person, phrase: string) {
-    console.log(`${phrase} ${this.name}`);
-  }
-}
-
-let user1: Person;
-
-user1 = new Person("Max");
-
-user1.greet("Hi I'm ");
+type Combinaable = string | number;
+type Numeric = number | boolean
+// 객체 타입이아닌 union 타입간에 intersection 타입은 
+// 두 타입중 중복되는 타입만 허용한다.
+type Universal = Combinaable & Numeric;
