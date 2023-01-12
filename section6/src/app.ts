3430caa85aea692d733e90094b5f9c4b6c5efdcc
JungHoe <junghwi0603@gmail.com>
2023-01-12 11:09:38 +0900

    section6 fixtypo

--- section6/src/app.ts
+++ section6/src/app.ts
@@ -3,24 +3,29 @@ type Admin = {
   priviliges: string[];
 };
 
-
 type Employee = {
-  name:string;
-  startDate : Date;
-}
+  name: string;
+  startDate: Date;
+};
 // & 연산자로 intersection 타입을 선언할수있다.
 type ElevatedEmployee = Admin & Employee;
 // interface 로 구현시 위 2가지 타입을 extends 받는것과 동일하다.
 
-
-const e1 : ElevatedEmployee = {
-  name : 'Junghoe',
-  priviliges: ['create-server'],
-  startDate: new Date();
-}
+const e1: ElevatedEmployee = {
+  name: "Junghoe",
+  priviliges: ["create-server"],
+  startDate: new Date(),
+};
 
 type Combinaable = string | number;
-type Numeric = number | boolean
-// 객체 타입이아닌 union 타입간에 intersection 타입은 
+type Numeric = number | boolean;
+// 객체 타입이아닌 union 타입간에 intersection 타입은
 // 두 타입중 중복되는 타입만 허용한다.
 type Universal = Combinaable & Numeric;
+
+function add(a: Combinaable, b: Combinaable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
