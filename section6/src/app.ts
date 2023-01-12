9171735e9afd4958991bba1702243395bbbac12c
JungHoe <junghwi0603@gmail.com>
2023-01-12 17:21:42 +0900

    section6 Type Guard
     * typeof
     * in
     * instanceof

--- section6/src/app.ts
+++ section6/src/app.ts
@@ -1,6 +1,6 @@
 type Admin = {
   name: string;
-  priviliges: string[];
+  privileges: string[];
 };
 
 type Employee = {
@@ -13,7 +13,7 @@ type ElevatedEmployee = Admin & Employee;
 
 const e1: ElevatedEmployee = {
   name: "Junghoe",
-  priviliges: ["create-server"],
+  privileges: ["create-server"],
   startDate: new Date(),
 };
 
@@ -24,8 +24,51 @@ type Numeric = number | boolean;
 type Universal = Combinaable & Numeric;
 
 function add(a: Combinaable, b: Combinaable) {
+  // 유니온타입의 동작을 명확하게해주는 type 검사를
+  // type Guard 라고 한다.
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmpInfo(emp: UnknownEmployee) {
+  console.log(`emp.name === ${emp.name}`);
+  if ("privileges" in emp) {
+    console.log(`privileges === ${emp.privileges}`);
+  }
+  if ("startDate" in emp) {
+    console.log(`startDate === ${emp.startDate}`);
+  }
+}
+printEmpInfo({ name: "Max", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("driving...");
+  }
+}
+class Truck {
+  drive() {
+    console.log("driving... Truck");
+  }
+  loadCargo(amount: number) {
+    console.log("loading cargo ..." + amount);
+  }
+}
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(veheicle: Vehicle) {
+  veheicle.drive();
+  if (veheicle instanceof Truck) {
+    veheicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
