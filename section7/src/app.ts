9b50fde6d3e6c664575efc3d37d19e2eaec21ab9
JungHoe <junghwi0603@gmail.com>
2023-01-16 15:06:05 +0900

    section7 generic 구현 및 제약조건
    * extends 키워드 사용하여 타입 제한 가능

--- section7/src/app.ts
+++ section7/src/app.ts
@@ -1,12 +1,38 @@
-// const names: string[] = [];
-const names: Array<string> = [];
+// // const names: string[] = [];
+// const names: Array<string> = [];
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("done done done");
-  }, 5000);
-});
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("done done done");
+//   }, 5000);
+// });
 
-promise.then((data) => {
-  data.split(" ");
-});
+// promise.then((data) => {
+//   data.split(" ");
+// });
+
+// const merge = (a: object, b: object) => {
+//   return Object.assign(a, b);
+// };
+
+// const mergedObj = merge({ name: "Max" }, { age: 30 });
+// mergedObj.name; //typescript는 해당object를 추론할수없음
+
+// const merge = <T, U>(a: T, b: U) => {
+//   return { ...a, ...b };
+// };
+
+// const mergedObj = merge<{ name: string }, { age: number }>(
+//   { name: "Max" },
+//   { age: 30 }
+// );
+
+const merge = <T extends object, U extends object>(a: T, b: U) => {
+  return Object.assign(a, b);
+};
+
+const mergedObj = merge<{ name: string }, { age: number }>(
+  { name: "Max" },
+  { age: 30 }
+);
+console.debug("🤔 ~ file: app.ts:36 ~ mergedObj", mergedObj);
